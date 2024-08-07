baf18f2425aeba4b3fbefc01493ba55105e34591
alexelwin <courechna@hotmailcom>
2024-08-07 12:48:13 +1000

    added dto 'name'

---
+++ Alex.MinimalApi.Service/Core/Name.cs
@@ -0,0 +1,21 @@
+﻿namespace Alex.MinimalApi.Service.Core
+{
+    //Encapsulate full name
+    public class Name
+    {
+        /// <summary>
+        /// firstname
+        /// </summary>
+        public string Firstname { get; set; }
+
+        /// <summary>
+        /// Lastname
+        /// </summary>
+        public string Lastname { get; set; }
+
+        ///<summary>
+        /// Title e.g. Mr, Ms
+        /// </summary>
+        public string title { get; set; }
+    }
+}
